5ccbdf60a09734d34c308e1cbc0a9f251e57e959
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-09-10 18:37:50 +0200

    _common.rb added.

--- cookbooks/mirna/attributes/default.rb
+++ cookbooks/mirna/attributes/default.rb
@@ -1,2 +1,17 @@
 default.dir.wf = "/opt/wf"
 default.dir.data = "/opt/data"
+
+default.data.readset = [
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX098%2FSRX098325/SRR346516/SRR346516.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX098%2FSRX098326/SRR346517/SRR346517.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX098%2FSRX098327/SRR346518/SRR346518.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX098%2FSRX098328/SRR346519/SRR346519.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130115/SRR445991/SRR445991.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130116/SRR445992/SRR445992.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130117/SRR445993/SRR445993.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130118/SRR445994/SRR445994.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130119/SRR445995/SRR445995.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130120/SRR445996/SRR445996.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130121/SRR445997/SRR445997.sra",
+  "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX130%2FSRX130122/SRR445998/SRR445998.sra"
+]
--- cookbooks/mirna/recipes/default.rb
+++ cookbooks/mirna/recipes/default.rb
@@ -4,9 +4,24 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, Caner Bagci, Jens Allmer, All Rights Reserved.
 
+read_dir = "#{node.dir.data}/reads"
+
 directory node.dir.wf
 directory node.dir.data
+directory read_dir
 
+# download read set
+
+node.data.readset.each { |read|
+
+  remote_file "#{read_dir}/#{File.basename( read )}" do
+    action :create_if_missing
+    source read
+  end
+}
+
+# place workflow
 template "#{node.dir.wf}/mirna.cf" do
   source "mirna.cf.erb"
 end
+
