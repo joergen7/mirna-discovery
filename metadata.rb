31473301a17e86fef48210070af5bc76f0e9399e
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-05 11:39:12 +0200

    Metadata updated.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,14 @@
 name             'mirna'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       "Jörgen Brandt"
+maintainer_email "brandjoe@hu-berlin.de"
 license          'all_rights'
 description      'Installs/Configures mirna'
 long_description 'Installs/Configures mirna'
 version          '0.1.0'
 
+recipe "mirna-discovery::_common", "Performs an update of the package system."
+recipe "mirna-discovery::default", "Sets up all preconditions to run the miRNA discovery workflow."
+
+
+depends "chef-bioinf-worker"
+depends "chef-cuneiform"
